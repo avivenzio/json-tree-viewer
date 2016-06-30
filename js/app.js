2fd6c086de36fded29cab6a082682ec04c019b3e
avivenzio <alex.vivenzio@gmail.com>
2016-06-29 22:35:50 -0400

    Added html and app.js file

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 (function(){
-	
 
+	//Create the tree
+	Tree.createTree("body",800,800);
 
-});
+	//Get Data and set in tree module
+	d3.json("http://swapi.co/api/starships/", function(error, data) {
+		Tree.setData(DataConversion.toNodes(data));
+	});
+
+})();
