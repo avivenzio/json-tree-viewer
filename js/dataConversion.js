fe6e74af068676b211a8cff37a6da76abcb04966
avivenzio <alex.vivenzio@gmail.com>
2016-07-26 21:26:34 -0400

    Convert primitives to reference types
    Fix bug where duplicate primitives were ignored due to being compared as
    equals. Changed to reference types.

--- js/dataConversion.js
+++ js/dataConversion.js
@@ -15,8 +15,16 @@ DataConversion= (function(){
 
 	Node.prototype.setTypeFromValue = function(obj){
 		var type = typeof obj;
-		if(type === 'object' && Array.isArray(obj))
-			type = "array";
+		if(type === 'object'){
+			if(Array.isArray(obj))
+				type = "array";
+			else if (obj instanceof String)
+				type = "string";
+			else if (obj instanceof Number)
+				type = "number";
+			else if (obj instanceof Boolean)
+				type = "boolean";
+		}
 		this.type= type;
 	};
 
@@ -25,18 +33,34 @@ DataConversion= (function(){
 		visitSet.add(node.data);
 		var newNode= new Node(node.name,[],node.data);
 		//Check type and place non objects as data
-		if(typeof node.data != 'object')
+		//if(typeof node.data != 'object')
+		if(typeof node.data != 'object' || node.data instanceof String || node.data instanceof Number || node.data instanceof Boolean)
 			newNode.children.push(new Node(node.data,[],node.data));
 		nodeStack.push(newNode);
 		parent.children.push(newNode);
 	};
 
+	function convertToReference(value){
+		if (typeof value ==='object')
+			return value;
+		else if (typeof value ==='string')
+			return new String(value);
+		else if (typeof value ==='number')
+			return new Number(value);
+		else if (typeof value ==='boolean')
+			return new Boolean(value);
+	};
+
 	//Find a property that has not been processed
 	function getUnvisitedProp(obj){
-		if(typeof obj ==='object' && obj!=null){
+		if(typeof obj ==='object' && obj!=null && !(obj instanceof String) && !(obj instanceof Number) && !(obj instanceof Boolean)){
+		//if(typeof obj ==='object' && obj!=null){
 			var keylist=Object.keys(obj);
 			for(var i=0; i < keylist.length; i++){
+				//make Reference types
+				obj[keylist[i]] = convertToReference(obj[keylist[i]]);
 				var currObj= obj[keylist[i]];
+				console.log(obj);
 				if(!visitSet.has(currObj)){
 					return { //return if not in set
 						data:currObj,
@@ -72,6 +96,7 @@ DataConversion= (function(){
 					nodeStack.pop();
 				}
 			}
+			console.log(visitSet);
 			return results;
 		}
 	};
